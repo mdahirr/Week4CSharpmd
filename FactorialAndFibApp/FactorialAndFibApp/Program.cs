cabe73d3bb24a104c6f9a2062ebfb46a8304a204
mdahirr <m.dahir@hotmail.co.uk>
2022-12-01 13:42:50 +0000

    recurssion and factorial exercise added

---
+++ FactorialAndFibApp/FactorialAndFibApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FactorialAndFibApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"factorial iterative: {FactorialIterative(5)}");
+            Console.WriteLine($"factorial recurrsive: {FactorialRecursive(5)}");
+            Console.WriteLine($"fib iterative: {FibonacciIterative(8)}");
+            Console.WriteLine($"fib recurrsive: {FibonacciRecursive(8)}");
+        }
+
+        #region Factorial iterative
+        public static int FactorialIterative(int n)
+        {
+            int result = 1;
+            for (int i = n; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        #endregion
+
+        #region Factorial Recursive
+
+        public static int FactorialRecursive(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * FactorialRecursive( n - 1);
+        }
+        #endregion
+
+        #region fib iterative
+
+        public static int FibonacciIterative(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                sum = i + (i + 1);
+            }
+            return sum;
+        }
+        #endregion
+
+        #region fib Recursive
+        public static int FibonacciRecursive(int n)
+        {
+            if ( n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+            }
+
+        }
+        #endregion
+                
+    }
+}
---
+++ Recurssion/Recurssion/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Recurssion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SumLoop of 5: {SumLoop(5)}");
+            Console.WriteLine($"SumRecursive of 5: {SumRecursive(5)}");
+        }
+
+        // recursive method
+
+        public static int SumRecursive(int n)
+        {
+            if (n == 0) return 0;
+            int prev = SumRecursive(n - 1);
+            int sum = n + prev;
+            return sum;
+        }
+
+        //iterative method
+
+        public static int SumLoop(int n) 
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+       
+    }
+}
